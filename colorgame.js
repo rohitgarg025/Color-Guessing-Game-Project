5f96bdfbad19b223b9ba63d6309f3ea4eaeb4480
Rohit Garg <rohitgarg025@gmail.com>
2020-07-06 16:38:08 +0530

    Added random number picker and Correct option functionality

--- colorgame.js
+++ colorgame.js
@@ -7,11 +7,13 @@ var colors = ["rgb(255, 0, 0)",
     "rgb(0, 255, 255)",
     "rgb(255, 0, 255)"
 ];
-var pickedColor = colors[2];
+
+//picks a random color from array
+var pickedColor = colors[Math.floor(Math.random()* colors.length)];
 
 var sqaure = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
-
+var messageDisplay = document.querySelector("#messageDisplay")
 colorDisplay.textContent = pickedColor
 
 for(var i = 0;i<6;i++){
@@ -19,12 +21,24 @@ for(var i = 0;i<6;i++){
 
     sqaure[i].addEventListener("click",function(){
         if(pickedColor === this.style.backgroundColor){
-            alert("Correct");
+            messageDisplay.textContent = "Correct";
+            changeColors(this.style.backgroundColor);
         }
         else{
-            alert("Wrong");
+            //alert("Wrong");
+            this.style.backgroundColor =  "#232323";
+            messageDisplay.textContent = "Try Again";
+
         }
     })
+}
+
+function changeColors(color){
+    for(var i=0;i<colors.length;i++){
+
+        //change each color to match given color
+        sqaure[i].style.backgroundColor = color;
+    }
 }
 
 
